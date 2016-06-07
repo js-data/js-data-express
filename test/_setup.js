5ead3ea020830de851d4c624a1c770294c491463
Dave <david.x.ramirez@jefferson.edu>
2016-06-07 11:13:47 -0400

    Added tests for queryParser, mount and Router

--- test/_setup.js
+++ test/_setup.js
@@ -1,11 +1,13 @@
 import * as JSDataExpress from '../src/index'
 import * as JSData from 'js-data'
+import {queryParser} from '../src/queryParser'
 import {assert} from 'chai'
 import sinon from 'sinon'
 
 export {
   JSDataExpress,
   JSData,
+  queryParser,
   assert,
   sinon
 }
